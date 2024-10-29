091dfe108cd95a17a5146c18680d73f0b054e26e
Kiana <kianakooshesh16@gmail.com>
2024-10-28 22:00:17 -0400

    ✅ Front-end of Web Apps Midterm Completed

---
+++ Midterm-Frontend/src/components/About.js
@@ -0,0 +1,8 @@
+const About = () => {
+    return <h1>Contact Me</h1>;
+};
+
+/* Responsible for exporting the About component for
+ use in the routing configuration in App.js */
+ 
+export default About;
